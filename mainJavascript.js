42810f3c56f81453824eda3c1243d91069a95abc
gaskahil <gaskahilde@gmail.com>
2025-06-01 08:03:33 +0200

    Corrected top of page, adjusted sizing for shop+ask, removed stray underline
    There are several elements I've cleaned up and as part of making the shop and ask part share the space on the page, I've connected the two elements to the same css code, saving me some lines of code there.
    
    I've created a mainpage for javascript as well.

---
+++ mainJavascript.js
@@ -0,0 +1,4 @@
+//open or close the menu panel
+document.querySelector(".side-panel-toggle").addEventListener("click", () => {
+  document.querySelector(".wrapper").classList.toggle("side-panel-open");
+});
